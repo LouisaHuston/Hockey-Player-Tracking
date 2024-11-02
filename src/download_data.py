e8ee4369ce9e5bc1926a6c8db99711851308293c
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-11-02 12:45:25 -0400

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,6 +1,8 @@
+import os
 
-def download_hockey_dataset():
-
-    # Check to see if the data is in the right directory, if not clone repo and move data
-
-    pass
+def download_data(repo_url, output_dir):
+    """Clone a Git repository to the specified directory."""
+    if not os.path.exists(output_dir):
+        os.system(f"git clone {repo_url} {output_dir}")
+    else:
+        print("Data already exists.")
