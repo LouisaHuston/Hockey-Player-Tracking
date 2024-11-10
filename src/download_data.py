194ddf32c89c501992c45a8926612d35b75f61fc
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-11-10 15:32:10 -0500

    Update download_data.py

--- src/download_data.py
+++ src/download_data.py
@@ -1,15 +1,10 @@
-# src/download_data.py
+import os
+import subprocess
 
-def download_dataset():
-    import os
-    import subprocess
-
-    dataset_dir = 'hockeyTrackingDataset'
+def download_dataset(dataset_dir):
 
     if not os.path.exists(dataset_dir):
         subprocess.run(['git', 'clone', 'https://github.com/grant81/hockeyTrackingDataset.git'])
         print("Dataset downloaded.")
     else:
         print("Dataset already downloaded.")
-
-    return os.path.abspath(dataset_dir)
