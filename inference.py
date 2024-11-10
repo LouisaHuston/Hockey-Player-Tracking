de108c7760858612b084bd6c5003e42aa0bf7d3a
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-11-10 15:26:44 -0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,1 +1,23 @@
+def main():
 
+  # 1) Extract frames from a video path you specify
+
+  # 2) Specify video relative location to the rink
+
+  # 3) Run inference on those frames - save result in COCO format to data/annoations/inference.json
+  
+  # 4) Use data/annoations/inference.json to predict locations for each of the bboxes for each of the frames
+
+  # 5) Use those predicted lcoations to identify where each person is on the rink - make a picture for each frame 
+
+  # 6) Turn all those frames into a movie
+
+  pass
+
+  
+
+  
+  pass
+
+if __name__ == '__main__':
+  main()
