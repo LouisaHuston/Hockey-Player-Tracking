efcfc7a6a63527e9c7cbd17d83022a2aabc6565f
LouisaHuston <115299047+LouisaHuston@users.noreply.github.com>
2024-11-10 14:52:09 -0500

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,1 +1,42 @@
+# src/evaluation.py
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 
+def calculate_accuracy(y_true, y_pred):
+    """
+    Calculate accuracy.
+    
+    :param y_true: List or array of true labels
+    :param y_pred: List or array of predicted labels
+    :return: Accuracy score
+    """
+    return accuracy_score(y_true, y_pred)
+
+def calculate_precision(y_true, y_pred):
+    """
+    Calculate precision.
+    
+    :param y_true: List or array of true labels
+    :param y_pred: List or array of predicted labels
+    :return: Precision score
+    """
+    return precision_score(y_true, y_pred, average='weighted', zero_division=1)
+
+def calculate_recall(y_true, y_pred):
+    """
+    Calculate recall.
+    
+    :param y_true: List or array of true labels
+    :param y_pred: List or array of predicted labels
+    :return: Recall score
+    """
+    return recall_score(y_true, y_pred, average='weighted', zero_division=1)
+
+def calculate_f1_score(y_true, y_pred):
+    """
+    Calculate F1 score.
+    
+    :param y_true: List or array of true labels
+    :param y_pred: List or array of predicted labels
+    :return: F1 score
+    """
+    return f1_score(y_true, y_pred, average='weighted', zero_division=1)
