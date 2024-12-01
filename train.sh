8b0e90129d96a1271809931e6ade027475de5ad3
AndrewTKent <57512897+AndrewTKent@users.noreply.github.com>
2024-12-01 16:56:25 -0500

    Update

--- train.sh
+++ train.sh
@@ -6,13 +6,28 @@ mkdir -p hockey
 # Export the CUDA home directory
 export CUDA_HOME=/usr/local/cuda
 
-# Always train on one GPU with the specific config
-echo "Starting training..."
-if ! python3 train.py configs/co_dino_hockey/co_dino_5scale_swin_large_16e_o365tococo.py --work-dir "hockey"; then
-  echo "Error: Python script execution failed."
-  kill $PID1
-  exit 1
-fi
+# Set the runtime variables
+WORK_DIR="hockey"
+RUNTIME_CONFIG_FILE="configs/co_dino_hockey/co_dino_5scale_swin_large_16e_o365tococo.py"
 
-# Ensure that the sync process is terminated after the script completes:
-kill $PID1
+# Get the number of available GPUs using nvidia-smi
+RUNTIME_GPUS=$(nvidia-smi --query-gpu=gpu_name --format=csv,noheader | wc -l)
+echo "Detected $RUNTIME_GPUS GPUs."
+
+# Check if number of GPUs is greater than 1
+if [ "$RUNTIME_GPUS" -gt 1 ]; then
+    # Run the main Python training distributed script
+    echo "Starting distributed training..."
+    if ! python3 -m torch.distributed.launch --nproc_per_node=$RUNTIME_GPUS --master_port=29300 \
+        $(dirname "$0")/train.py "$RUNTIME_CONFIG_FILE" --launcher pytorch --work-dir "$SAFETY_SET"; then
+      echo "Error: Distributed Python script execution failed."
+      exit 1
+    fi
+else
+    # Run the main Python training script
+    echo "Starting training..."
+    if ! python3 train.py "$RUNTIME_CONFIG_FILE" --work-dir "$SAFETY_SET"; then
+      echo "Error: Python script execution failed."
+      exit 1
+    fi
+fi
